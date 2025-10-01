3048351ef6995e022b6ed9767f960f2e5226f0f4
Hassan Jamil <hassanjamil91@gmail.com>
2025-10-02 03:28:38 +0400

    Initial Commit

---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by Hassan Jamil on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostsAppTests/PostsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppTests.swift
+//  PostsAppTests
+//
+//  Created by Hassan Jamil on 02/10/2025.
+//
+
+import Testing
+@testable import PostsApp
+
+struct PostsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
